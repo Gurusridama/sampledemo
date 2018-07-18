ce960b72f6a5c9ff6878d79c6438ec930940f434
Gurusridama <41327315+Gurusridama@users.noreply.github.com>
2018-07-18 10:55:57 +0530

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,1 +1,10 @@
+var define = require('node-constants')(exports)
+var mysql      = require('mysql');
+var dataConfig = {
+	server: "localhost",
+	user: "root",
+	password: "",
+	database : 'demodata'
+};
 
+define({dataConfig : dataConfig});
